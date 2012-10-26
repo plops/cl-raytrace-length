e983141dd84f718bf999b478f04d47c22a6f45f4
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-10-26 22:34:51 +0200

    improve things with single lens system
    make sure the last surface is hit
    reversal works
    i didn't refract on plane surfaces

--- dat.lisp
+++ dat.lisp
@@ -20,11 +20,13 @@
 	(thick-old 0))
     (loop for (curv ind thick) in sys and i from 0 collect
 	 (prog1
-	
-	   (list curv ind (if (= i 0) 
-			      start
-			     (incf start thick-old)))
+	     (list curv ind (incf start thick-old))
 	   (setf thick-old thick)))))
+
+#+nil
+(unsplit-optical-system `((0 50 -30 0) (1 1.5 1) (100 10 100)))
+#+nil
+(accum-thickness (unsplit-optical-system `((0 50 -30 0) (1 1.5 1) (100 10 100))))
 
 (defun calc-centers (sys)
   (loop for (curv ind vert) in sys collect
@@ -103,11 +105,17 @@
 
 (defun unsplit-optical-system (sys3 &key (outside-index 1d0))
   (destructuring-bind (curvs inds thicks) sys3
-    (loop for c in curvs 
-       and i in inds
-       and d in thicks
-       collect
-	(list c i d))))
+    (unless (= (1- (length curvs))
+	       (length inds)
+	       (length thicks))
+      (error "system doesn't contain a correct number of parameters"))
+    (loop for i below (length curvs) collect
+	 (if (< i (length inds))
+	  (list (elt curvs i) (elt inds i) (elt thicks i))
+	  (list (elt curvs i) (elt inds (1- i)) 0d0)))))
+
+#+nil
+(unsplit-optical-system `((0 50 -30 0) (1 1.5 1) (100 10 100)))
 
 #+nil
 (unsplit-optical-system ;; zeiss 40x 0..2mm coverslip
@@ -116,24 +124,6 @@
     (1.5205 1.5891 1.6134 1.4866 1.8807 1.5285 1.4585 1.6967)
     (3.24 1.43 2.50 0.62 1.0 4.7 1.0 0.2 3.0 15.75 4.0 1.5)))
 
-(defparameter *zeiss100* 
-  (unsplit-optical-system ;; zeiss US2009/0284841 cheap planachromat
-  ;; tubelength 200, 100x NA1.25
-  ;; visual field factor 20, .28 mm working distance
-  ;; curv index thick
-   `(,(append '(0d0 0d0 0d0)
-	     (mapcar #'/ '(-1.9845 -32.1451 -5.8498 -87.1808 10.0650
-			   -16.4805 16.4805 -10.0650 87.1808 8.8700
-			   4.4577 2.4528 -2.4528 -4.4577))
-	     '(0d0 0d0))
-     ,(mapcar #'(lambda (x) (+ 1d0 (/ x 1000d0))) 
-	      '(525 518 517 0 758 0 762 667 0 667 762 0 489 813 0 813 0 0 0))
-     (.17 .281 2.770 .2 2.2 7.127 3. 4. .2 4. 3. 4.823 6.5 4. 1.8 4. .5 100 1))))
-
-
-(accum-thickness *zeiss100*)
-
-(calc-centers (accum-thickness *zeiss100*))
 
 (defun reverse-optical-system (sys)
   (let ((s3 (split-optical-system sys)))
@@ -368,179 +358,227 @@ asin (/ 1.25 1.525)))) ;; aperture half-angle
 
 #.(require :cl-who)
 
+(accum-thickness *zeiss100*)
+
+(calc-centers (accum-thickness *zeiss100*))
+
+(calc-centers
+ (accum-thickness (reverse-optical-system *zeiss100*)))
+
 #+nil
 (progn
- (let ((sys *zeiss100*))
-   (defparameter *sys* sys)
-   
-   (defparameter *bla* ;; margin ray
-     (let* ((start (v 0 0 0.0d0))
-	    (dir-x 0d0)
-	    (dir-y (/ 1.25d0 1.525)
-	     )
-	    (dir (v dir-x dir-y (sqrt (- 1 (+ (expt dir-x 2)
-					      (expt dir-y 2))))))
-	    (old-index 1.525d0))
-       (loop for (curv index center-z) in
-	    (calc-centers (accum-thickness sys))
-	  collect	   
-	    (prog1
-		(if (= curv 0)
-		    (intersect-plane start dir (v 0 0 center-z)
-				     (v 0 0 -1))
-		    (multiple-value-bind (start-new dir-new)
-			(trace-ray start dir (v 0 0 center-z)
-				   (/ curv)
-				   (/ old-index index))
-		      (setf dir dir-new
-			    start start-new)))
-	      (setf old-index index)))))
 
-   (defparameter *bla3* ;; coma ray
-     (let* ((start (v 0 .1596d0 0.0d0))
-	    (dir-x 0d0)
-	    (dir-y (/ -.5d0 1.525)
-	     )
-	    (dir (v dir-x dir-y (sqrt (- 1 (+ (expt dir-x 2)
-					      (expt dir-y 2))))))
-	    (old-index 1.525d0))
-       (loop for (curv index center-z) in
-	    (calc-centers (accum-thickness sys))
-	  collect	   
-	    (prog1
-		(if (= curv 0)
-		    (intersect-plane start dir (v 0 0 center-z)
-				     (v 0 0 -1))
-		    (multiple-value-bind (start-new dir-new)
-			(trace-ray start dir (v 0 0 center-z)
-				   (/ curv)
-				   (/ old-index index))
-		      (setf dir dir-new
-			    start start-new)))
-	      (setf old-index index)))))
-#+nil
-   (defparameter *bla2* ;; chief ray
-     (let* ((start (v 0 0 0))
-	    (dir-x 0d0)
-	    (dir-y (/ 10.d0 200d0) ;; y=12.5 mm in intermediate is 202um in sample
-	     )
-	    (dir (v dir-x dir-y (sqrt (- 1 (+ (expt dir-x 2)
-					      (expt dir-y 2))))))
-	    (syss (reverse-optical-system *zeiss100*))
-	    (old-index 1.0d0))
-       (defparameter *sysss* syss)
-       (loop for (curv index center-z) in
-	    (calc-centers (accum-thickness syss))
-	  collect	   
-	    (prog1
-		(if (= curv 0)
-		    (intersect-plane start dir (v 0 0 center-z)
-				     (v 0 0 -1))
-		    (multiple-value-bind (start-new dir-new)
-			(trace-ray start dir (v 0 0 center-z)
-				   (/ curv)
-				   (/ old-index index))
-		      (setf dir dir-new
-			    start start-new)))
-	      (setf old-index index)))))
-   #+nil
-   (with-open-file (s "/dev/shm/o.dat" :direction :output
+ #+nil (defparameter *zeiss100* 
+    (unsplit-optical-system ;; zeiss US2009/0284841 cheap planachromat
+     ;; tubelength 200, 100x NA1.25
+     ;; visual field factor 20, .28 mm working distance
+     ;; curv index thick
+     `(,(append '(0d0 0d0 0d0)
+		(mapcar #'/ '(-1.9845 -32.1451 -5.8498 -87.1808 10.0650
+			      -16.4805 16.4805 -10.0650 87.1808 8.8700
+			      4.4577 2.4528 -2.4528 -4.4577))
+		'(0d0))
+	,(mapcar #'(lambda (x) (+ 1d0 (/ x 1000d0))) 
+		 '(525 518 517 0 758 0 762 667 0 667 762 0 489 813 0 813 0 0))
+	(.17 .281 2.770 .2 2.2 7.127 3. 4. .2 4. 3. 4.823 6.5 4. 1.8 4. .5 100))))
+
+  (defparameter *zeiss100* 
+    (unsplit-optical-system ;; curv index thick
+     `(,(append '(0d0)
+		(mapcar #'/ '(20d0))
+		'(0d0 0d0))
+	,(mapcar #'(lambda (x) (+ 1d0 (/ x 1000d0))) 
+		 '(0 500 0))
+	(100 3 100))))
+
+
+  (defparameter *bla* nil)
+  (defparameter *bla2* nil)
+  (defparameter *bla3* nil)
+
+  (let ((sys *zeiss100*))
+    (defparameter *sys* sys)
+    
+    (defparameter *bla* ;; margin ray
+      (let* ((start (v 0 5d0 0.0d0))
+	     (dir-x 0d0)
+	     (dir-y 0d0 ;(/ 1.25d0 1.525)
+	       )
+	     (dir (v dir-x dir-y (sqrt (- 1 (+ (expt dir-x 2)
+					       (expt dir-y 2))))))
+	     (old-index 1d0))
+	(loop for (curv index center-z) in
+	     (calc-centers (accum-thickness sys))
+	   collect	   
+	     (prog1
+		 (if (= curv 0)
+		     (intersect-plane start dir (v 0 0 center-z)
+				      (v 0 0 -1))
+		     (multiple-value-bind (start-new dir-new)
+			 (trace-ray start dir (v 0 0 center-z)
+				    (/ curv)
+				    (/ old-index index))
+		       (setf dir dir-new
+			     start start-new)))
+	       (setf old-index index)))))
+
+    #+nil
+    (defparameter *bla3* ;; coma ray
+      (let* ((start (v 0 .1596d0 0.0d0))
+	     (dir-x 0d0)
+	     (dir-y (/ -.5d0 1.525)
+	       )
+	     (dir (v dir-x dir-y (sqrt (- 1 (+ (expt dir-x 2)
+					       (expt dir-y 2))))))
+	     (old-index 1.525d0))
+	(loop for (curv index center-z) in
+	     (calc-centers (accum-thickness sys))
+	   collect	   
+	     (prog1
+		 (if (= curv 0)
+		     (intersect-plane start dir (v 0 0 center-z)
+				      (v 0 0 -1))
+		     (multiple-value-bind (start-new dir-new)
+			 (trace-ray start dir (v 0 0 center-z)
+				    (/ curv)
+				    (/ old-index index))
+		       (setf dir dir-new
+			     start start-new)))
+	       (setf old-index index)))))
+
+    
+    (defparameter *bla2* ;; chief ray
+      (let* ((start (v 0 -2d0 0))
+	     (dir-x 0d0)
+	     (dir-y 0d0 ;(/ 10.d0 200d0) ;; y=12.5 mm in intermediate is 202um in sample
+	       )
+	     (dir (v dir-x dir-y (sqrt (- 1 (+ (expt dir-x 2)
+					       (expt dir-y 2))))))
+	     (syss (reverse-optical-system *zeiss100*))
+	     (old-index (second (first syss))))
+	(defparameter *sysss* syss)
+	(loop for (curv index center-z) in
+	     (calc-centers (accum-thickness syss))
+	   collect	   
+	     (prog1
+		 (if (= curv 0)
+		     (let* ((n (v 0 0 -1))
+			    (start-new (intersect-plane start dir (v 0 0 center-z)
+							n))
+			    (dir-new (refract-plane dir n (/ old-index index))))
+		       (setf dir dir-new
+			     start start-new))
+		     (multiple-value-bind (start-new dir-new)
+			 (trace-ray start dir (v 0 0 center-z)
+				    (/ curv)
+				    (/ old-index index))
+		       (setf dir dir-new
+			     start start-new)))
+	       (format t "~a~%" (list (list curv index center-z) start dir))
+	       (setf old-index index)))))
+    #+nil
+    (with-open-file (s "/dev/shm/o.dat" :direction :output
+		       :if-does-not-exist :create
+		       :if-exists :supersede)
+      (dolist (e (nth
+		  (floor (length *bla*) 2)
+		  *bla*))
+	(dolist (p e)
+	  (format s "~f ~f~%" (aref p 2) (aref p 0)))
+	(terpri s))))
+
+
+
+
+
+  (with-open-file (st "/dev/shm/o.svg" :direction :output
 		      :if-does-not-exist :create
 		      :if-exists :supersede)
-     (dolist (e (nth
-		 (floor (length *bla*) 2)
-		 *bla*))
-       (dolist (p e)
-	 (format s "~f ~f~%" (aref p 2) (aref p 0)))
-       (terpri s))))
-
-
-
- (with-open-file (st "/dev/shm/o.svg" :direction :output
-		     :if-does-not-exist :create
-		     :if-exists :supersede)
-   (format st "~a" "<?xml version='1.0' encoding='iso-8859-1'?>")
-   (cl-who:with-html-output (s st :indent t)
-     (cl-who:htm 
-      (:svg :width "5000px" :height "5000px"
-	    :version "1.1" :xmlns "http://www.w3.org/2000/svg" :|xmlns:xlink| "http://www.w3.org/1999/xlink" 
-	    :|viewBox| "0 0 1200 600" ;:style "enable-background new 0 0 1200 600;" :|xml:space| "preserve"
-	    (cl-who:str (loop for  (curv index center-z) in
-			     (calc-centers (accum-thickness  *zeiss100*))
-			   do
-			     (let* ((sc 10)
-				    (h (* sc 7)))
-			       (let ((xe (+ 20 (* sc 48.571)))
-				     (rpupil (let* ((m 100) ;; pupil radius is 2.5
-						    (ftl 200)
-						    (f (/ ftl m))
-						    (na 1.25))
-					       (* f na))))
-				(cl-who:htm (:line :x1 20 :y1 200 ;; optical axis
-						   :x2 5000 :y2 200
-						   :style "stroke:black; stroke-width:.1px;")
-					    (:line :x1 100 :y1 (+ 200 (* sc 2.5)) ;; axis at 2.5mm
-						   :x2 300 :y2 (+ 200 (* sc 2.5))
-						   :style "stroke:black; stroke-width:.1px;")
-					    (:line :x1 xe :y1 (+ 200 (* sc h)) ;; top stop
-						   :x2 xe :y2 (+ 200 (* sc rpupil))
-						   :style "stroke:black; stroke-width:2px;")
-					    (:line :x1 xe :y1 (- 200 (* sc h)) ;; bottom stop
-						   :x2 xe :y2 (- 200 (* sc rpupil))
-						   :style "stroke:black; stroke-width:2px;")))
-			       
-			       (loop for (color f) in `(("green" ,*bla*) ("blue" ,*bla3*)) do
-				    (loop for i from 1 below (length f) and e in f do
-					 (let ((x1 (format nil "~a" (+ 20 (* sc (aref e 2)))))
-					       (y1 (format nil "~a" (+ 200 (* sc (aref e 1)))))
-					       (x2 (format nil "~a" (+ 20 (* sc (aref (elt f i) 2)))))
-					       (y2 (format nil "~a" (+ 200 (* sc (aref (elt f i) 1))))))
-					   (cl-who:htm (:line :x1 x1 :y1 y1
-							      :x2 x2 :y2 y2
-							      :style (format nil "stroke:~a;stroke-width:.1px"
-									     color))))))
-			       #+nil
-			       (loop for i from 1 below (length *bla2*) and e in *bla2* do
-				    (let ((x1 (format nil "~a" (+ 20 (* sc (- 48.571 (aref e 2))))))
-					  (y1 (format nil "~a" (+ 200 (* sc (aref e 1)))))
-					  (x2 (format nil "~a" (+ 20 (* sc (- 48.571 (aref (elt *bla2* i) 2))))))
-					  (y2 (format nil "~a" (+ 200 (* sc (aref (elt *bla2* i) 1))))))
-				      (cl-who:htm (:line :x1 x1 :y1 y1
-							 :x2 x2 :y2 y2
-							 :style "stroke:red;stroke-width:.1px"))))
-			       (if (= 0 curv)
-				   (let ((x (format nil "~a" (+ 20 (* sc center-z)))))
-				     (cl-who:htm (:line :x1 x :y1 (format nil "~a" (- 200 h))
-							:x2 x :y2 (format nil "~a" (+ 200 h))
-							:style "stroke:black;")))
-				   
-				   
-				   (cl-who:htm 
+    (format st "~a" "<?xml version='1.0' encoding='iso-8859-1'?>")
+    (cl-who:with-html-output (s st :indent t)
+      (cl-who:htm 
+       (:svg :width "5000px" :height "5000px"
+	     :version "1.1" :xmlns "http://www.w3.org/2000/svg" :|xmlns:xlink| "http://www.w3.org/1999/xlink" 
+	     :|viewBox| "0 0 1200 600" ;:style "enable-background new 0 0 1200 600;" :|xml:space| "preserve"
+	     (cl-who:str (loop for  (curv index center-z) in
+			      (calc-centers (accum-thickness  *zeiss100*))
+			    do
+			      (let* ((sc 12)
+				     (h (* sc 7)))
+				(let ((xe (+ 20 (* sc 48.571)))
+				      (rpupil (let* ((m 100) ;; pupil radius is 2.5
+						     (ftl 200)
+						     (f (/ ftl m))
+						     (na 1.25))
+						(* f na))))
+				  (cl-who:htm (:line :x1 20 :y1 200 ;; optical axis
+						     :x2 5000 :y2 200
+						     :style "stroke:black; stroke-width:.1px;")
+					      (:line :x1 100 :y1 (+ 200 (* sc 2.5)) ;; axis at 2.5mm
+						     :x2 300 :y2 (+ 200 (* sc 2.5))
+						     :style "stroke:black; stroke-width:.1px;")
+					      (:line :x1 xe :y1 (+ 200 (* sc h)) ;; top stop
+						     :x2 xe :y2 (+ 200 (* sc rpupil))
+						     :style "stroke:black; stroke-width:2px;")
+					      (:line :x1 xe :y1 (- 200 (* sc h)) ;; bottom stop
+						     :x2 xe :y2 (- 200 (* sc rpupil))
+						     :style "stroke:black; stroke-width:2px;")))
+				
+				(loop for (color f) in `(("green" ,*bla*) ("blue" ,*bla3*)) do
+				     (when f
+				       (loop for i from 1 below (length f) and e in f do
+					    (let ((x1 (format nil "~a" (+ 20 (* sc (aref e 2)))))
+						  (y1 (format nil "~a" (+ 200 (* sc (aref e 1)))))
+						  (x2 (format nil "~a" (+ 20 (* sc (aref (elt f i) 2)))))
+						 (y2 (format nil "~a" (+ 200 (* sc (aref (elt f i) 1))))))
+					     (cl-who:htm (:line :x1 x1 :y1 y1
+								:x2 x2 :y2 y2
+								:style (format nil "stroke:~a;stroke-width:.1px"
+									       color)))))))
+				
+				(when *bla2*
+				 (loop for i from 1 below (length *bla2*) and e in *bla2* do
+				      (let* ((len (aref (first (last *bla2*)) 2))
+					     (x1 (format nil "~a" (+ 20 (* sc (- len (aref e 2))))))
+					     (y1 (format nil "~a" (+ 200 (* sc (aref e 1)))))
+					     (x2 (format nil "~a" (+ 20 (* sc (- len (aref (elt *bla2* i) 2))))))
+					    (y2 (format nil "~a" (+ 200 (* sc (aref (elt *bla2* i) 1))))))
+					(cl-who:htm (:line :x1 x1 :y1 y1
+							   :x2 x2 :y2 y2
+							   :style "stroke:red;stroke-width:.1px")))))
+				(if (= 0 curv)
+				    (let ((x (format nil "~a" (+ 20 (* sc center-z)))))
+				      (cl-who:htm (:line :x1 x :y1 (format nil "~a" (- 200 h))
+							 :x2 x :y2 (format nil "~a" (+ 200 h))
+							 :style "stroke:black;")))
 				    
-				    #+nil (:circle :cx (+ 200 center-z)
-						   :cy 200
-						   :r (/ (abs curv))
-						   :style "stroke: red; stroke-width: 3; fill:none;")
-				    (:g :stroke "black" :fill "none" 
-					(:path :d (let* ((r (* sc (-  (/ curv))))
-							 (cz (* sc center-z))
-							 ;; (alpha (* (- 180 20) pi (/ 180)))
-							 (alpha (if (< h (abs r))
-								(- pi (sin (/ h (abs r))))
-								(* (- 180 90) pi (/ 180))
-								))
-							 (x (- (+ r (* r (cos alpha)))))
-							 (y (* -1 r (sin alpha))))
-						    (concatenate 'string
-								 ;; (rx ry x-axis-rotation large-arc-flag sweep-flag x y)+
-								 (format nil "M ~a 200 a ~a ~a 0 0 0 ~a ~a"
-									 (+ 20 r cz)
-									 r r
-									 x y)
-								 (format nil "M ~a 200 a ~a ~a 0 0 1 ~a ~a"
-									 (+ 20 r cz)
-									 r r
-									 x (- y))))))))))))))))
+				    
+				    (cl-who:htm 
+				     
+				     #+nil (:circle :cx (+ 200 center-z)
+						    :cy 200
+						    :r (/ (abs curv))
+						    :style "stroke: red; stroke-width: 3; fill:none;")
+				     (:g :stroke "black" :fill "none" 
+					 (:path :d (let* ((r (* sc (-  (/ curv))))
+							  (cz (* sc center-z))
+							  ;; (alpha (* (- 180 20) pi (/ 180)))
+							  (alpha (if (< h (abs r))
+								 (- pi (sin (/ h (abs r))))
+								 (* (- 180 90) pi (/ 180))
+								 ))
+							  (x (- (+ r (* r (cos alpha)))))
+							  (y (* -1 r (sin alpha))))
+						     (concatenate 'string
+								  ;; (rx ry x-axis-rotation large-arc-flag sweep-flag x y)+
+								  (format nil "M ~a 200 a ~a ~a 0 0 0 ~a ~a"
+									  (+ 20 r cz)
+									  r r
+									  x y)
+								  (format nil "M ~a 200 a ~a ~a 0 0 1 ~a ~a"
+									  (+ 20 r cz)
+									  r r
+									  x (- y))))))))))))))))
 
 
 
